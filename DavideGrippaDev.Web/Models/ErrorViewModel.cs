e1b60130587e484a388bae79ddd4c88b1af1cd0f
davidino <davide.grippa2001@icloud.com>
2022-09-26 23:18:56 +0200

    Initial commit

---
+++ DavideGrippaDev.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DavideGrippaDev.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
